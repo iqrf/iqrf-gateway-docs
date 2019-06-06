d11f8b5714f08bc31d7caa0cd6ff9a72f2835554
Rostislav Spinar <rostislav.spinar@iqrf.com>
2019-06-06 04:35:59 +0200

    Fix version

--- developer/source/conf.py
+++ developer/source/conf.py
@@ -26,7 +26,7 @@ author = 'IQRF Tech s.r.o.'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = 'v2.1.3'
+release = 'v2.1.4'
 
 
 # -- General configuration ---------------------------------------------------
